ab462e4c2c7905669b30f1027925ebdd4f6d2677
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-18 12:53:41 +0800

    Add initial route paths and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <h1>This is a test</h1>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
 import App from './App';
+import HomePage from './pages/HomePage';
+
+import './css/index.css';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<HomePage />} />
+    </Route>,
+  ),
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
