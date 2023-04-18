749469d5bc0f5c9c7a0c797bf54e3b052dbfb7d4
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-18 18:19:03 +0800

    Provide link to HomePage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <header className="flex justify-between p-4 bg-slate-500">
-      <button type="button" className="flex-col">
+      <Link to="/" className="flex-col">
         <h1 className="text-2xl font-poppins font-extrabold">Hidden Hunt</h1>
         <h2>Search for characters</h2>
-      </button>
+      </Link>
       <div className="flex">
         <button type="button" className="bg-red-300">
           Info
