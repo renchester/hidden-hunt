b41dbf58b2a21683b3dd895e28aa47bb0049ffe1
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-19 18:40:29 +0800

    Render Header within HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,21 @@
+import Header from '../components/Header';
 import GameMapCard from '../components/GameMapCard';
 
 import gameData from '../data/gameData';
 
 function HomePage() {
   return (
-    <main className="max-w-[1600px] mx-auto">
-      <h1>Choose a map to play on</h1>
-
-      <section className="flex gap-6">
-        {gameData.map((map) => (
-          <GameMapCard map={map} key={`${map.id}-card`} />
-        ))}
-      </section>
-    </main>
+    <>
+      <Header />
+      <main className="max-w-[1600px] mx-auto">
+        <h1>Choose a map to play on</h1>
+        <section className="flex flex-col gap-16 px-10">
+          {gameData.map((map) => (
+            <GameMapCard map={map} key={`${map.id}-card`} />
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
 export default HomePage;
