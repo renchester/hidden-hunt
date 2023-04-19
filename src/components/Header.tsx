280db3e2c913257d6af342b190c65ec05b02b4c5
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-19 18:39:15 +0800

    Move Header component render location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="flex justify-between p-4 bg-slate-500">
+    <header className="sticky top-0 z-50 w-full flex justify-between p-4 bg-slate-500">
       <Link to="/" className="flex-col">
         <h1 className="text-2xl font-poppins font-extrabold">Hidden Hunt</h1>
         <h2>Search for characters</h2>
