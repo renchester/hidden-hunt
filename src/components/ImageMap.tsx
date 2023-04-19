5dd6b8afa8543e304e29ec70a66c4f63cee965fd
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-19 16:07:22 +0800

    Add ImageMap component

---
+++ src/components/ImageMap.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from 'react';
+
+import getCoordsFromDB from '../api/getCoordsFromDB';
+import { useCharacters } from '../hooks/useCharacters';
+import isWithinDegrees from '../utils/isWithinDegrees';
+
+import { type Map, type MapType, type Coordinates } from '../types/types';
+import ContextMenu from './ContextMenu';
+
+type ImageMapProps = {
+  currentGame: Map;
+  mapType: MapType;
+};
+
+function ImageMap(props: ImageMapProps) {
+  const { currentGame, mapType } = props;
+
+  const { characters, setCharacters } = useCharacters();
+  const [isMenuShown, setMenuShowStatus] = useState(false);
+  const [clickedCoordinates, setClickedCoordinates] = useState<Coordinates>();
+
+  const mapRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    console.log(characters);
+
+    if (characters.every((ch) => ch.isFound)) {
+      console.log('all found');
+    }
+  }, [characters]);
+
+  const matchCharacterToCoords = async (clickedCharacterId: string) => {
+    const characterCoords = await getCoordsFromDB(mapType, clickedCharacterId);
+    const DELTA = 3;
+
+    if (
+      clickedCoordinates &&
+      characterCoords &&
+      isWithinDegrees(DELTA, clickedCoordinates.x, characterCoords.x) &&
+      isWithinDegrees(DELTA, clickedCoordinates.y, characterCoords.y)
+    ) {
+      // Set character isFound to true
+      setCharacters(
+        characters.map((mapChar) =>
+          mapChar.id === clickedCharacterId
+            ? {
+                ...mapChar,
+                isFound: true,
+              }
+            : mapChar,
+        ),
+      );
+
+      //  Print Found
+      console.log(`Found ${clickedCharacterId}`);
+    } else {
+      // Print Try Again
+      console.log('try again');
+    }
+  };
+
+  const handleImageClick = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+  ) => {
+    const mapRect = mapRef.current && mapRef.current.getBoundingClientRect();
+
+    if (mapRect) {
+      const clickX = ((e.clientX - mapRect.x) / mapRect.width) * 100;
+      const clickY = ((e.clientY - mapRect.y) / mapRect.height) * 100;
+
+      setClickedCoordinates(() => ({ x: clickX, y: clickY }));
+      setMenuShowStatus(() => true);
+    }
+  };
+
+  const hideMenu = () => setMenuShowStatus(false);
+
+  return (
+    <div className="relative" onBlur={hideMenu}>
+      <div className="expandable absolute"></div>
+      <img
+        ref={mapRef}
+        src={currentGame.imgSource}
+        alt={`Map for ${currentGame.title}`}
+        className="mx-auto"
+        onClick={handleImageClick}
+      />
+      {isMenuShown && clickedCoordinates && (
+        <ContextMenu
+          hideMenu={hideMenu}
+          clickedCoordinates={clickedCoordinates}
+          matchCharacterToCoords={matchCharacterToCoords}
+        />
+      )}
+    </div>
+  );
+}
+export default ImageMap;
