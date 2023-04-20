9c959d7888afbb15b235e4fc25b3e039787060c8
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-20 14:05:09 +0800

    Add hideModal prop to Overlay component

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,8 +1,18 @@
 import type { ReactNode } from 'react';
 
-function Overlay({ children }: { children?: ReactNode }) {
+type OverlayProps = {
+  children?: ReactNode;
+  hideModal?: () => void;
+};
+
+function Overlay(props: OverlayProps) {
+  const { children, hideModal } = props;
+
   return (
-    <div className="fixed z-20 top-0 bottom-0 left-0 right-0 bg-black bg-opacity-40">
+    <div
+      className="fixed z-20 top-0 bottom-0 left-0 right-0 bg-black bg-opacity-40"
+      onClick={hideModal}
+    >
       {children}
     </div>
   );
