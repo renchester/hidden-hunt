712df47459b1eb848f181b4b5bcc6de9a518fee5
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-20 17:54:40 +0800

    Add getTime util function

---
+++ src/utils/getTime.ts
@@ -0,0 +1,15 @@
+function getTime(time: number) {
+  const centiseconds = Math.floor((time / 10) % 100);
+  const seconds = Math.floor((time / 1000) % 60);
+  const minutes = Math.floor((time / 60000) % 60);
+  const hours = Math.floor(time / 360000);
+
+  return {
+    seconds,
+    centiseconds,
+    minutes,
+    hours,
+  };
+}
+
+export default getTime;
