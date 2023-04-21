ff7c0fb0cddf730ac262ce1404e9b5191e6edbac
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-21 22:03:25 +0800

    Fix bug with wrong hour calculation

--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -2,7 +2,7 @@ function getTime(time: number) {
   const centiseconds = Math.floor((time / 10) % 100);
   const seconds = Math.floor((time / 1000) % 60);
   const minutes = Math.floor((time / 60000) % 60);
-  const hours = Math.floor(time / 360000);
+  const hours = Math.floor(time / 3600000);
 
   return {
     seconds,
